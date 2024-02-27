c848e630124737915f78a509156b297c0a69471f
Zhang, Chaojun <chaojun.zhang@intel.com>
2024-02-27 10:02:48 +0000

    init commit of fraud detection serve

--- fraud_detect/serve/serve.py
+++ fraud_detect/serve/serve.py
@@ -6,9 +6,6 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", dest="config", type=str, required=True)
-    parser.add_argument("-a", "--app", dest="app_name", type=str, required=False, default='credit_card')
     args = parser.parse_args()
     config = Config.load(args.config)
-    config.app_name = args.app_name
-
     run_serve(config)
